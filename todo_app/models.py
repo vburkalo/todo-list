a53b32ae34ae5ce9aec0ec65d556f2dfcc00044c
vital <vitaliyburkalo81@gmail.com>
2024-05-01 17:42:01 +0300

    updated models for the project

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tags", blank=True)
+
+    def __str__(self):
+        return self.content
