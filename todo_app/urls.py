f6370409fd7ab4a74262aa6509d4901a7379b454
vital <vitaliyburkalo81@gmail.com>
2024-05-01 17:59:04 +0300

    updated todo app urls to have better reverse link names

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -15,16 +15,16 @@ app_name = "todo_list"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("task/create/", TaskCreateView.as_view(), name="task_create"),
-    path("task/update/<int:task_id>/", TaskUpdateView.as_view(), name="task_update"),
-    path("task/delete/<int:task_id>/", TaskDeleteView.as_view(), name="task_delete"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/update/<int:task_id>/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/delete/<int:task_id>/", TaskDeleteView.as_view(), name="task-delete"),
     path(
         "task/toggle/<int:task_id>/",
         TaskToggleDoneView.as_view(),
-        name="task_toggle_done",
+        name="task_toggle-done",
     ),
-    path("tags/", TagListView.as_view(), name="tag_list"),
-    path("tag/create/", TagCreateView.as_view(), name="tag_create"),
-    path("tag/update/<int:tag_id>/", TagUpdateView.as_view(), name="tag_update"),
-    path("tag/delete/<int:tag_id>/", TagDeleteView.as_view(), name="tag_delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/update/<int:tag_id>/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/delete/<int:tag_id>/", TagDeleteView.as_view(), name="tag-delete"),
 ]
