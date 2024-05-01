c26aaa9d8579bfd7e4c37883c244c2094d6f137f
vital <vitaliyburkalo81@gmail.com>
2024-05-01 18:53:50 +0300

    update tag model to have str method

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField()
